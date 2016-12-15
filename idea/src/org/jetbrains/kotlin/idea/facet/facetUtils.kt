b29023ef193bde8b66eefd9e55256833877446fb
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2016-12-15 18:44:35 +0100

    Fix compilation

--- idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
@@ -34,6 +34,7 @@ import org.jetbrains.kotlin.idea.compiler.configuration.KotlinCompilerSettings
 import org.jetbrains.kotlin.idea.framework.JSLibraryStdPresentationProvider
 import org.jetbrains.kotlin.idea.framework.JavaRuntimePresentationProvider
 import org.jetbrains.kotlin.idea.framework.getLibraryProperties
+import org.jetbrains.kotlin.idea.versions.bundledRuntimeVersion
 
 private fun getRuntimeLibraryVersions(
         module: Module,
@@ -127,8 +128,8 @@ fun KotlinFacetSettings.initializeIfNeeded(module: Module, rootModel: ModuleRoot
 val TargetPlatformKind<*>.mavenLibraryId: String
     get() {
         return when (this) {
-            is JVMPlatform -> "kotlin-stdlib"
-            is JSPlatform -> "kotlin-js-library"
+            is TargetPlatformKind.Jvm -> "kotlin-stdlib"
+            is TargetPlatformKind.JavaScript -> "kotlin-js-library"
             else -> error("Unexpected platform: $this")
         }
     }
